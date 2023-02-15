a10a790163663047e876c599c6400960f72fe990
Rafael Faria <82117664+RafaFernande@users.noreply.github.com>
2023-02-14 21:54:18 -0300

    Add files via upload

---
+++ programacao estrut/programas/teste/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nome[100];
+    printf("Digite o seu nome: ");
+    scanf("%s",nome);/*Entrada de dados*/
+    printf("Ola, %s",nome);
+    return 0;
+}
